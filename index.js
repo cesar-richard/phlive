66fdfc2a7d14b679f58aea3b2f5645834692035b
Cesar <licorne@utc.fr>
2020-04-30 15:00:43 +0200

    A

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
-const number = 50;
+const number = 5;
 let c = 0;
 async function startLive(browser, instance) {
   setTimeout(async () => {
@@ -13,18 +13,18 @@ async function startLive(browser, instance) {
     await page
       .goto(url, { waitUntil: "networkidle2", timeout: 60000 })
       .catch(e => console.error(`[${instance}]`, e.message));
-    console.log(`[${instance}]`, "Video loaded");
+  //  console.log(`[${instance}]`, "Video loaded");
     await page.waitFor(".ytp-button");
     //await page.waitFor(10000).catch(e => console.error(`[${instance}]`, e.message));
-    console.log(`[${instance}]`, "Click on play button");
+    //console.log(`[${instance}]`, "Click on play button");
     await page
       .waitFor(3000)
       .catch(e => console.error(`[${instance}]`, e.message));
-    await page
-      .screenshot({
-        path: `screenshots/live-${instance}-1.png`
-      })
-      .catch(e => console.error(`[${instance}]`, e.message));
+    // await page
+    //   .screenshot({
+    //     path: `screenshots/live-${instance}-1.png`
+    //   })
+    //   .catch(e => console.error(`[${instance}]`, e.message));
     page.keyboard
       .down("K")
       .catch(e => console.error(`[${instance}]`, e.message));
@@ -33,14 +33,20 @@ async function startLive(browser, instance) {
       .catch(e => console.error(`[${instance}]`, e.message));
     await page
       .screenshot({
-        path: `screenshots/live-${instance}-2.png`,
-        fullPage: true
+        path: `screenshots/live-${instance}-2.png`
       })
       .catch(e => console.error(`[${instance}]`, e.message));
+    const inter = setInterval(async function () {
+      const vc = await page.$('.view-count');
+      const svc = await page.$('.short-view-count');
+      const vct = await page.evaluate(e=> e.textContent, vc);
+      const svct = await page.evaluate(e=> e.textContent, svc);
+      console.info(`[${instance}]`, `${vct} - (${svct})`)
+    }, 2000);
     await page
       .waitFor(130000)
       .catch(e => console.error(`[${instance}]`, e.message));
-
+    clearInterval(inter);
     c++;
     if (c == number || true) {
       console.log(`[${instance}]`, "Closing browser");
@@ -55,7 +61,8 @@ async function startLive(browser, instance) {
 }
 
 async function test() {
-  puppeteer.launch({ headless: true }).then(async browser => {
+  puppeteer.launch({ headless: true,executablePath:
+    "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" }).then(async browser => {
     console.log("Running tests..");
     const page = await browser.newPage();
     await page.goto("https://bot.sannysoft.com");
@@ -82,9 +89,9 @@ async function goLive(number) {
       .launch({
         headless: true,
         executablePath:
-          "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+          "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
         //"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-        //defaultViewport: null,
+        defaultViewport: null,
         //args: ["--no-sandbox"]
       })
       .catch(e => console.error(`[${number}]`, e.message));
@@ -94,5 +101,5 @@ async function goLive(number) {
   }
 }
 
-test();
+//test();
 goLive(number);
